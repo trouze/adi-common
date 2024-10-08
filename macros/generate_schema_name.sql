972e1d371976c53537f5175189b95b297efee50d
Tyler Rouze <tyler@tylerrouze.com>
2024-10-08 18:05:32 -0500

    Update generate_schema_name.sql

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,4 +1,4 @@
-{% macro adi_common__generate_schema_name(custom_schema_name, node) -%}
+{% macro default__generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
 
