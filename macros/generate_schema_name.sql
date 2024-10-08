a899f13df0dfe86c0a5b2332fc170d59366a7d62
Tyler Rouze <tyler@tylerrouze.com>
2024-10-08 18:21:38 -0500

    Update generate_schema_name.sql

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -25,3 +25,87 @@
         {%- endif -%}
     {% endif %}
 {%- endmacro %}
+
+{% macro snowflake__generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+
+    {%- if env_var('DBT_CLOUD_ENVIRONMENT_TYPE') == 'dev' -%}
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+
+        {%- else -%}
+
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+
+        {%- endif -%}
+
+    {% else %}
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+
+        {%- else -%}
+
+            {{ custom_schema_name | trim }}
+
+        {%- endif -%}
+    {% endif %}
+{%- endmacro %}
+
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+
+    {%- if env_var('DBT_CLOUD_ENVIRONMENT_TYPE') == 'dev' -%}
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+
+        {%- else -%}
+
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+
+        {%- endif -%}
+
+    {% else %}
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+
+        {%- else -%}
+
+            {{ custom_schema_name | trim }}
+
+        {%- endif -%}
+    {% endif %}
+{%- endmacro %}
+
+{% macro adi_common__generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+
+    {%- if env_var('DBT_CLOUD_ENVIRONMENT_TYPE') == 'dev' -%}
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+
+        {%- else -%}
+
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+
+        {%- endif -%}
+
+    {% else %}
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+
+        {%- else -%}
+
+            {{ custom_schema_name | trim }}
+
+        {%- endif -%}
+    {% endif %}
+{%- endmacro %}
