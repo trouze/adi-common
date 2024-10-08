b9940278b12772e4bdf633b7e3818963d72b4437
Tyler Rouze <tyler@tylerrouze.com>
2024-10-08 18:47:13 -0500

    Update generate_schema_name.sql

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,4 +1,32 @@
-{% macro snowflake__generate_schema_name(custom_schema_name, node) -%}
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {{ log("Custom generate_schema_name called!", info=true) }}
+    {%- set default_schema = target.schema -%}
+
+    {%- if env_var('DBT_CLOUD_ENVIRONMENT_TYPE') == 'dev' -%}
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+
+        {%- else -%}
+
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+
+        {%- endif -%}
+
+    {% else %}
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+
+        {%- else -%}
+
+            {{ custom_schema_name | trim }}
+
+        {%- endif -%}
+    {% endif %}
+{%- endmacro %}
+
+{% macro default__generate_schema_name(custom_schema_name, node) -%}
     {{ log("Custom generate_schema_name called!", info=true) }}
     {%- set default_schema = target.schema -%}
 
